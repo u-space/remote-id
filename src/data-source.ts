4be93e90c287646c022c71434b1cd0c76fdf6cd2
emi <emialonzo@gmail.com>
2024-06-11 15:31:37 -0300

    export intialized promise

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,6 +1,10 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 import Configuration from "./utils/Configuration";
+import { Position } from "./entities/position";
+
+const entitiesPath = __dirname + "/entities/*{.js,.ts}";
+console.log(entitiesPath);
 
 export const AppDataSource = new DataSource({
   type: "postgres",
@@ -10,8 +14,19 @@ export const AppDataSource = new DataSource({
   password: Configuration.DATABASE_PASSWORD,
   database: Configuration.DATABASE_DATABASE,
   synchronize: true,
+  dropSchema: true,
   logging: false,
-  entities: [__dirname + "/entities/*{.js,.ts}"],
+  entities: [Position],
+  // entities: [__dirname + "./src/entities/*{.js,.ts}"],
   migrations: [],
   subscribers: [],
+  ssl: true,
 });
+
+export const initialized = AppDataSource.initialize()
+  .then(() => {
+    console.log("Data Source has been initialized!");
+  })
+  .catch((err) => {
+    console.error("Error during Data Source initialization", err);
+  });
